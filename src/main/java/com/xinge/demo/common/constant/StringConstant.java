8ca191331c6f70e37b2f3d64f6cf9ddc408eb969
duanxq <duanxiuqing1994@foxmail.com>
2018-01-23 12:24:29 +0800

    删除无用代码

--- src/main/java/com/xinge/demo/common/constant/StringConstant.java
+++ src/main/java/com/xinge/demo/common/constant/StringConstant.java
@@ -8,99 +8,16 @@ package com.xinge.demo.common.constant;
  */
 public class StringConstant {
 
-
     public static final String COOKIE_COMPANY_ID = "COMPANY_ID";
 
     public static final String COOKIE_EMPLOYEE_ID = "EMPLOYEE_ID";
 
     public static final String COOKIE_EMPLOYEE_TYPE = "EMPLOYEE_TYPE";
 
-    public static final String COOKIE_ZED_MOBILE = "ZED_MOBILE";
-
-    public static final String COOKIE_ZED_SHOP_ID = "ZED_SHOP_ID";
-
     public static final String SESSION_USER = "adminer";
-
-    public static final String SESSION_VALIDATE_CODE = "validateCode";
-
-    public static final String SESSION_IMAGE_CODE = "imageCode";
 
     public static final String REQUEST_MAP = "requestResMap";
 
-    public static final String REDIRECT_URL_APPLY_ID_CARD = "apply_id_card";
-
-    public static final String REDIRECT_URL_APPLY_INFO = "apply_info";
-
-    public static final String REDIRECT_URL_APPLY_PHOTO = "apply_photo";
-
-    public static final String REDIRECT_URL_APPLY_DETAIL = "apply_detail";
-
     public static final String PRODUCT_DEFAULT_VALUE = "-1";
-
-    //联网报警编号
-    public static final String PRODUCT_LIAN_WANG_DEFAULT_VALUE = "1";
-
-    public static final String FENGXIAOLIANG_MOBILE = "18069790998";
-
-    public static final String XIONGGUO_MOBILE = "15757127843";
-
-    public static final String YAOCHENGYA_MOBILE = "15757133106";
-
-    public static final String SMS_MESSAGE_CONTENT = "您的短信密码为%s,请不要告知他人【中标金服】";
-
-    public static final String SMS_FIRST_CHECK_CONFIRM = "验证码：%s（请勿泄露）业务员{name}正在为您提交联网报警服务申请，请确认！【中标金服】";
-
-    public static final String SMS_CHECK_INSTALLMENT_CONFIRM = "验证码：%s，小心周围人偷瞄噢！您正在开通联网报警服务按月付费业务，请确认。【中标金服】";
-
-    public static final String SMS_FIRST_CHECK_SUCCESS_ALL = "业务员为%s提交了一笔%s申请，请及时审核！【中标金服】";
-
-    public static final String SMS_FIRST_CHECK_NOTPASS_SYSTEM = "很抱歉，您为%s提交的%s资料未能通过初审，请重新提交审核！【中标金服】";
-    public static final String SMS_FIRST_CHECK_NOTPASS_MESSAGE = "很抱歉，您为%s提交的联网报警服务资料未能通过初审，请重新提交审核！【中标金服】";
-
-    public static final String SMS_FIRST_CHECK_FAILL_SYSTEM = "很抱歉，您为%s提交的联网报警服务资料已被否决，请告知商户！【中标金服】";
-    public static final String SMS_FIRST_CHECK_FAILL_MESSAGE = "很抱歉，您为%s提交的%s资料已被否决，请告知商户。如有疑问请致电客服：0571-89808771！【中标金服】";
-
-    public static final String SMS_FIRST_CHECK_PASS = "您好，您为%s提交的联网报警服务资料已通过初审，我们会在第一时间内进行复审，不要着急，马上就好！【中标金服】";
-
-    public static final String SMS_SECOND_CHECK_CHECKER_NOTPASS_SYSTEM = "%s业务员为%s办理的联网报警服务申请未通过复审！【中标金服】";
-    public static final String SMS_SECOND_CHECK_EMPLOYEE_NOTPASS_MESSAGE = "很抱歉，您为%s提交的%s资料未能通过复审，请重新提交审核，如有疑问请致电客服：0571-89808771！【中标金服】";
-    public static final String SMS_SECOND_CHECK_EMPLOYEE_NOTPASS_SYSTEM = "很抱歉，您为%s提交的联网报警服务资料未能通过复审，打开软件可查看详细原因，极大提高通过概率噢！【中标金服】";
-
-    public static final String SMS_SECOND_CHECK_EMPLOYEE_PASS_MESSAGE = "您好，您为%s提交的%s资料已通过复审，请及时为商户安装设备！【中标金服】";
-    public static final String SMS_SECOND_CHECK_EMPLOYEE_PASS_SYSTEM = "太棒啦！您为%s提交的联网报警服务资料已通过复审，快出发为商户安装设备吧！【中标金服】";
-    public static final String SMS_SECOND_CHECK_CHECKER_PASS_SYSTEM = "%s业务员为%s办理的联网报警服务申请通过复审！【中标金服】";
-    public static final String SMS_SECOND_CHECK_BUSINESS_PASS_SYSTEM = "尊敬的%s，您的联网报警服务申请已通过复审！【中标金服】";
-    public static final String SMS_SECOND_CHECK_BUSINESS_PASS_MESSAGE = "尊敬的%s，您的%s按月付费请求已通过！【中标金服】";
-
-    public static final String SMS_INSTALLMENT_CONFIRM = "尊敬的%s，您的联网报警服务还款已全部结清，祝您生活愉快！【中标金服】";
-    public static final String SMS_INSTALLMENT_EMPLOYEE_PASS_SYSTEM = "您好，%s的联网报警服务已办理成功，请知悉！【中标金服】";
-    public static final String SMS_INSTALLMENT_EMPLOYEE_PASS_MESSAGE = "您好，%s的%s已办理成功，请知悉！【中标金服】";
-    public static final String SMS_INSTALLMENT_CHECKER_PASS_SYSTEM = "您好，%s业务员为%s办理的联网报警服务已办理成功，请知悉！【中标金服】";
-    public static final String SMS_INSTALLMENT_CHECKER_PASS_MESSAGE = "您好，%s业务员为%s办理的%s已办理成功，请知悉！【中标金服】";
-    public static final String SMS_INSTALLMENT_BUSINESS_PASS_SYSTEM = "尊敬的%s，您的联网报警服务已办理成功。每月15日是您的还款日，共12期。祝您生活愉快！【中标金服】";
-    public static final String SMS_INSTALLMENT_BUSINESS_PASS_MESSAGE = "尊敬的%s，您的%s已办理成功。每月15日是您的还款日，共%s期。祝您生活愉快！【中标金服】";
-
-    public static final String SMS_INSTALLMENT_EMPLOYEE_LATE = "%s的联网报警服务业务已过期，快抓紧时间争取续约吧，GO，GO，GO！【中标金服】";
-    public static final String SMS_INSTALLMENT_CHECKER_LATE = "%s业务员为%s办理的联网报警服务已过期！【中标金服】";
-    public static final String SMS_INSTALLMENT_BUSINESS_LATE = "尊敬的%s，您的联网报警服务按月付费已过期，感谢您对中标金服平台的信赖与支持！【中标金服】";
-
-    public static final String SMS_THISINSTALLMENT_SYSTEM = "尊敬的%s，您本期联网报警服务按月付费待还款a元，请保证还款账户余额充足！【中标金服】";
-    public static final String SMS_THISINSTALLMENT_MESSAGE = "尊敬的%s，您本期%s按月付费待还款%.2f元，待还款将于本月15日自动扣款，请保证还款账户余额充足！【中标金服】";
-
-    public static final String SMS_REPLACE_FAILL_MESSAGE = "尊敬的%s，您本期%s应还金额%s元已经扣款成功！【中标金服】";
-    public static final String SMS_REPLACE_FAILL_SYSTEM = "尊敬的%s，您本期%s应还金额%s元已经扣款成功！【中标金服】";
-
-    public static final String SMS_REPLACE_SUCCESS_MESSAGE = "尊敬的%s，您本期%s应还金额%s元扣款失败。请保证您尾号%s的账户余额充足，以免影响您的征信!【中标金服】";
-    public static final String SMS_REPLACE_SUCCESS_SYSTEM = "尊敬的%s，您本期%s应还金额%s元扣款失败。请保证您尾号%s的账户余额充足，以免影响您的征信！【中标金服】";
-
-    public static final String SMS_REPLACE_FAILL_SEVENDAY = "%s的联网报警服务还款已逾期超过7天！【中标金服】";
-
-
-    public static final String SMS_REPAYMENT_FINISH_SYSTEM = "尊敬的%s，您的联网报警服务还款已全部结清，祝您生活愉快！【中标金服】";
-    public static final String SMS_REPAYMENT_FINISH_MESSAGE = "尊敬的%s，您的联网报警服务还款已全部结清，祝您生活愉快！【中标金服】";
-
-    public static final String SMS_REPAYMENT_OVERDUE_FIRSTDAY = "尊敬的%s，您的%s按月付费本期应还%s元已逾期，请您尽快向银行卡充值，以免影响您的信用。祝您生活愉快！【中标金服】";
-    public static final String SMS_REPAYMENT_OVERDUE_TENTHDAY = "尊敬的%s，您的%s按月付费本期费用已由安保公司垫付，请尽快联系安保公司工作人员进行缴费。祝您生活愉快！【中标金服】";
-
 
 }
