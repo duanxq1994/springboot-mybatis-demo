26b5e35d88a49efa11021807032f93a9869373a2
BG343674 <xqduan@best-inc.com>
2019-04-09 14:13:01 +0800

    数据库配置修改

--- src/test/java/com.xinge/demo/CodeGenerator.java
+++ src/test/java/com.xinge/demo/CodeGenerator.java
@@ -21,7 +21,7 @@ import static com.xinge.demo.ProjectConstant.*;
  */
 public class CodeGenerator {
     //JDBC配置，请修改为你项目的实际配置
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/paper";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test";
     private static final String JDBC_USERNAME = "root";
     private static final String JDBC_PASSWORD = "123456";
     private static final String JDBC_DIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
@@ -40,7 +40,7 @@ public class CodeGenerator {
     private static final String DATE = new SimpleDateFormat("yyyy/MM/dd").format(new Date());//@date
 
     public static void main(String[] args) {
-        genCode("member","order_sequence","paper_order","paper_price","paper_price_log","paper_type","supply_price","user");
+        genCode("user");
     }
 
     /**
