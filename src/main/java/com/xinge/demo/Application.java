a34393d7c4679b7209237a15bfeaa25f174da48e
BG343674 <xqduan@best-inc.com>
2019-04-19 16:38:02 +0800

    内置数据库H2

--- src/main/java/com/xinge/demo/Application.java
+++ src/main/java/com/xinge/demo/Application.java
@@ -1,5 +1,7 @@
 package com.xinge.demo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.h2.tools.Server;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +11,8 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.sql.SQLException;
+
 /**
  * @author duanxq
  */
@@ -17,13 +21,30 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableAsync
 @MapperScan("com.xinge.demo.mapper")
+@Slf4j
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
+        startH2Server();
         SpringApplication.run(Application.class, args);
     }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        startH2Server();
         return builder.sources(Application.class);
     }
+
+    private static void startH2Server() {
+        try {
+            Server h2Server = Server.createTcpServer().start();
+            if (h2Server.isRunning(true)) {
+                log.info("H2 server was started and is running.");
+            } else {
+                throw new RuntimeException("Could not start H2 server.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to start H2 server: ", e);
+        }
+    }
+
 }
---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,5 @@
+-- user:admin password:admin
+INSERT INTO `user` (`id`, `name`, `password`, `salt`, `status`, `remark`, `email`, `mobile`, `type`, `version`, `create_time`, `token`, `token_expire_time`, `deleted`)
+VALUES
+  ('1', 'admin', '6e5f801f273066d592c048b38e870d2a', 'RaGapN', '1', NULL, '', '', '2', '5', '2019-02-23 12:00:00', NULL,
+   NULL, '0');
